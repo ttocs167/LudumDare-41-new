2eff62ca56ea7f524334bd66bd36965790d65cdd
Carvster <38571305+Carvster@users.noreply.github.com>
2018-04-22 22:23:24 +0100

    Claude now has sound effcts

---
+++ Assets/ClaudeEffects.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClaudeEffects : MonoBehaviour {
+
+	private AudioSource ClaudeFX;
+
+	// Use this for initialization
+	void Start () {
+		ClaudeFX = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(){
+		
+		ClaudeFX.Play ();
+
+	}
+}
