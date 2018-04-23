2fc15587d8c189a03ddd88a10fe99a441e554a81
RobertMason96 <robert.mason343@gmail.com>
2018-04-23 02:57:35 +0100

    Health Follow
    Health Follow

---
+++ Assets/Scripts/healthFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class healthFollow : MonoBehaviour {
+    private Camera cam;    
+    private Transform target;
+    public Vector2 offset=new Vector2(0,0);
+    private string text="";
+    void Start()
+    {
+        cam = Camera.main;
+        target = this.gameObject.transform;
+    }   
+    public void setText(string tx)
+    {
+        text = tx;
+    }
+    void OnGUI()
+    {
+        Vector3 screenPos = cam.WorldToScreenPoint(target.position);
+        int sy = Screen.height - (int)screenPos.y;
+        GUI.Label(new Rect(screenPos.x+offset.x, sy+offset.y, 100, 20), "Hello World!");
+    }
+
+}
