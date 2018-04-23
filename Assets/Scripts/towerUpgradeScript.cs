c1727f52bbdceffc2364413356a1b7e4f32ce75e
StuPle <stuart.f.poole@gmail.com>
2018-04-23 18:40:06 +0100

    tower upgrade scripts working but no shader update on range change

--- Assets/Scripts/towerUpgradeScript.cs
+++ Assets/Scripts/towerUpgradeScript.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class towerUpgradeScript : MonoBehaviour {
 
     public string towerType;
     public int price;
+    public Text textbox;
     private GameObject player;
     private GameObject manager;
     private GameObject[] towers;
@@ -13,12 +15,29 @@ public class towerUpgradeScript : MonoBehaviour {
     void Start () {
         player = GameObject.FindGameObjectWithTag("Player");
         manager = GameObject.FindGameObjectWithTag("ManagerObject");
-        towers = GameObject.FindGameObjectsWithTag(towerType);
     }
-	
+
+    public void Update()
+    {
+        towers = GameObject.FindGameObjectsWithTag(towerType);
+        if (towerType == "BlueTower")
+        {
+            price = 200 * towers.Length;
+        }
+        else if (towerType == "SquidTower")
+        {
+            price = 300 * towers.Length;
+        }
+
+        textbox.text = "" + price;
+        Debug.Log("" + price);
+
+    }
+
 
     public void OnButtonPress()
     {
+
         if (manager.transform.gameObject.GetComponent<phaseManager>().currentState == "BUILD" & manager.transform.gameObject.GetComponent<BuildingManagerScript>().currentMoney >= price)
         {
             manager.transform.gameObject.GetComponent<BuildingManagerScript>().currentMoney -= price;
@@ -30,6 +49,7 @@ public class towerUpgradeScript : MonoBehaviour {
                     towers[i].transform.gameObject.GetComponent<towerBehaviour>().range += 1f;
 
                 }
+
             }
             else if (towerType == "SquidTower")
             {
@@ -38,8 +58,10 @@ public class towerUpgradeScript : MonoBehaviour {
                     towers[i].transform.gameObject.GetComponent<towerBehaviour>().rateOfFire += 0.2f;
                     towers[i].transform.gameObject.GetComponent<towerBehaviour>().range += 1f;
                     towers[i].transform.gameObject.GetComponent<towerBehaviour>().shotCount += 1;
+                    
 
                 }
+
             }
         }
     }
